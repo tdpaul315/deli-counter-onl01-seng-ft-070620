30f99de25b49d6b980e144019834f13633f7913a
Tiauna Paul <tiaunaj.tsg@gmail.com>
2020-07-14 05:47:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,3 +20,10 @@ def line(array)
   end 
 end
 
+def now_serving(array)
+  if array.empty? == true 
+    puts "There is nobody waiting to be served!"
+  elsif array.empty? == false 
+    puts "Currently serving #{array.shift}."
+  end 
+end 
